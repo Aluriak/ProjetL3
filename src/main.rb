f5d25d301d773c8b1d44a7dfce0be9ac4b2281bb
Aluriak <lucas.bourneuf@laposte.net>
2014-02-23 20:19:57 +0100

    Code utilisateur du Picross

---
+++ src/main.rb
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+# MAIN.RB
+# Using of Picross class
+# BOURNEUF
+ 
+#################################
+# IMPORTS			#
+#################################
+load 'src/picross/picross.rb'
+
+
+
+
+
+i = Picross.new
+
+
+
+
+
+
