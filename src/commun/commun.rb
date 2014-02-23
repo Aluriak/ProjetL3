c624210e97236216905a73135b85a70a7c4b1c3e
Aluriak <lucas.bourneuf@laposte.net>
2014-02-23 14:43:50 +0100

    définitions en commun

---
+++ src/commun/commun.rb
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+# COMMUN.RB
+# définition de types et fonctions communes au programme
+# BOURNEUF
+ 
+#################################
+# IMPORTS			#
+#################################
+
+
+
+
+
+#################################
+# GRILLE SIZE 			#
+#################################
+# mainteneur : BOURNEUF
+
+
+
+#################################
+# FUNCTIONS			#
+#################################
+
+
+
