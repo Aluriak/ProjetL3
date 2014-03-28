bfd9c6b444f73169bf7bf280c839cf6f7e02a147
Aluriak <lucas.bourneuf@laposte.net>
2014-03-28 15:46:36 +0100

    gds + algo de transformation de matrice de jeu en matrice d'entier

--- src/picross/picross.rb
+++ src/picross/picross.rb
@@ -13,7 +13,7 @@ load "grille/racine.rb"
 
 
 #################################
-# CLASSNAME			#
+# PICROSS			#
 #################################
 # mainteneur : BOURNEUF
 class Picross
@@ -38,7 +38,7 @@ class Picross
       @config = Configuration.new(1,5)
     end
     # Chargement d'une grille
-    #TODO
+    self.gestionnaireDeSauvegarde = GestionnaireDeSauvegarde.new
   end
 
 
@@ -60,6 +60,97 @@ class Picross
     return self
   end
 
+
+  # Création d'une grille racine à partir de la table d'état passée en argument.
+  # Aucun effet si la table n'est pas de la taille réglementaire.
+  def creerGrilleRacine(tableEtat) 
+    # substituer les états DRAPEAU par l'état BLANC
+    tableEtat.each do |ligne| 
+      ligne.each do |c| 
+      	if c == Etat.Drapeau then c = Etat.Blanc end
+      end
+    end
+
+    # IMPLÉMENTER LISTE DES COLONNES
+    matriceFacteursColonne = Array.new
+    # pour chacune des lignes
+    tableEtat.size.times do |col|
+      # compteur de noir
+      nbNoirConsecutif = 0 
+      # ajouter une liste d'entier à la matrice de facteurs
+      matriceFacteurColonne.push(Array.new)
+      # pour chacune des colonnes
+      tableEtat.size.times do |row|
+      	# obtenir l'état de la case
+      	c = tableEtat[row][col]
+	# si c'est du blanc
+      	if c == Etat.Blanc then 
+	  # et qu'il y a eu un noir avant
+	  if nbNoirConsecutif > 0 then			
+	    # insérer un entier dans la liste de la ligne actuelle
+	    matriceFacteursColonne.last.push(nbNoirConsecutif)	
+      	    nbNoirConsecutif = 0 
+	  end						
+	else
+	  # si c'est pas blanc, c'est noir
+      	  nbNoirConsecutif += 1				
+	end
+      end
+      # si la dernière case était un noir, cela n'a pas été ajouté à la liste
+      # on intègre donc le nombre de dernières cases noires
+      if nbNoirConsecutif > 0 then			
+	# insérer un entier dans la liste de la ligne actuelle
+	matriceFacteursColonne.last.push(nbNoirConsecutif)	
+      	nbNoirConsecutif = 0 
+      end						
+    end
+
+    # IMPLÉMENTER LISTE DES LIGNES
+    matriceFacteursLigne = Array.new
+    # pour chacune des lignes
+    tableEtat.size.times do |row|
+      # compteur de noir
+      nbNoirConsecutif = 0 
+      # ajouter une liste d'entier à la matrice de facteurs
+      matriceFacteurColonne.push(Array.new)
+      # pour chacune des colonnes
+      tableEtat.size.times do |col|
+      	# obtenir l'état de la case
+      	c = tableEtat[row][col]
+	# si c'est du blanc
+      	if c == Etat.Blanc then 
+	  # et qu'il y a eu un noir avant
+	  if nbNoirConsecutif > 0 then			
+	    # insérer un entier dans la liste de la ligne actuelle
+	    matriceFacteursLigne.last.push(nbNoirConsecutif)	
+      	    nbNoirConsecutif = 0 
+	  end						
+	else
+	  # si c'est pas blanc, c'est noir
+      	  nbNoirConsecutif += 1				
+	end
+      end
+      # si la dernière case était un noir, cela n'a pas été ajouté à la liste
+      # on intègre donc le nombre de dernières cases noires
+      if nbNoirConsecutif > 0 then			
+	# insérer un entier dans la liste de la ligne actuelle
+	matriceFacteursLigne.last.push(nbNoirConsecutif)	
+      	nbNoirConsecutif = 0 
+      end						
+    end
+
+    # CRÉER LA GRILLE RACINE
+    grilleRacine = GrilleRacine.creerDepuis(
+      Grille.new(matriceEtat.size, 
+      		 matriceFacteursLigne,
+		 matriceFacteursColonne),
+      self.idGrilleSuivant
+    )
+
+    # ENREGISTRER LA GRILLE RACINE
+    self.gestionnaireDeSauvegarde.ajouterGrilleRacine(grilleRacine)
+    
+  end
 
   # Remise à zéro de la configuration
   # METHODE DE DEBUG
