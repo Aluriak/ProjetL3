12728ba58da9a6b450248b1bb3cd2bfcaf9f3c63
Aluriak <lucas.bourneuf@laposte.net>
2014-03-28 15:45:48 +0100

    Constructeur modifié pour simplifier la création de grille

--- src/grille/grille.rb
+++ src/grille/grille.rb
@@ -29,19 +29,26 @@ class Grille
   attr_accessor :matriceDesColonnes
 
 
-  def initialize(taille)
-    # Les matrices ne contiennent de prime abord que des nils
-    
+  # Attend la taille de la grille.
+  # Si les matrices de lignes et colonnes sont renseignées, elles sont utilisées telles quelles
+  # Sinon, elles sont remplacées par des matrices sans entiers
+  def initialize(taille, matriceDesLignes = nil, matriceDesColonnes = nil)
     # matriceDesLignes[3][4] accède au 5ème chiffre de la 4ème ligne
-    @matriceDesLignes = Array.new(taille) do |index| 
-      Array.new(taille/2 + 1) { 0 }
+    if matriceDesLignes == nil then 
+      @matriceDesLignes = Array.new(taille) { Array.new }
+    else
+      @matriceDesLignes = matriceDesLignes
     end
 
     # matriceDesColonnes[0][10] accède au 11ème chiffre de la 1ère ligne
-    @matriceDesColonnes = Array.new(taille) do |index| 
-      Array.new(taille/2 + 1) { 0 }
+    if matriceDesColonnes == nil then 
+      @matriceDesColonnes = Array.new(taille) { Array.new } 
+    else
+      @matriceDesColonnes = matriceDesColonnes
     end
   end
+
+
 
   # Créer une Grille de taille voulue
   # Attend une taille de grille valable (== contenu dans Grille.taille)
