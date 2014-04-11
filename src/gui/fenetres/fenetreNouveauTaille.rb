06324534ce79cf2f4c80c4163798ca0e51fc08c6
LeCroissant <nico22330@gmail.com>
2014-04-11 13:46:27 +0200

    Ajout evenement lors de selection de radiobutton

--- src/gui/fenetres/fenetreNouveauTaille.rb
+++ src/gui/fenetres/fenetreNouveauTaille.rb
@@ -18,28 +18,43 @@ class FenetreNouveauTaille
 
 			vb = VBox.new(false, 6)
 			
-			vb.pack_start(b = RadioButton.new("5 x 5"))
-			vb.pack_start(b2 = RadioButton.new(b, "10 x 10"))
-			vb.pack_start(b3 = RadioButton.new(b, "15 x 15"))
-			vb.pack_start(b4 = RadioButton.new(b, "20 x 20"))
-			vb.pack_start(b5 = RadioButton.new(b, "25 x 25"))
+			vb.pack_start(b5 = RadioButton.new("5 x 5"))
+			vb.pack_start(b10 = RadioButton.new(b5, "10 x 10"))
+			vb.pack_start(b15 = RadioButton.new(b10, "15 x 15"))
+			vb.pack_start(b20 = RadioButton.new(b15, "20 x 20"))
+			vb.pack_start(b25 = RadioButton.new(b20, "25 x 25"))
 					
 
 			#Ajout de la 2eme horizontal box contenant boutons
 			hbBouton = HBox.new(false, 1)
 			hbBouton.pack_start(ok = Button.new(Stock::OK), true, true)
 			
-			#Evenement lors du click sur OK
-			ok.signal_connect("clicked"){
-				puts b.group
-				#On crée un nouveau jeu de picross avec pour taille, la taille du radiobutton selectionné
-				#Il faudra donc surcharger le constructeur de gui pour pouvoir y ajouter une taille de matrice
-				#Gui.lancer("Picross", @tailleNouvelleMatrice)
+						
 
-			}			
 			hbBouton.pack_start(cancel = Button.new(Stock::CLOSE), true, true)
 			cancel.signal_connect("clicked") { popupTailleGrille.destroy }
 			vb.pack_start(hbBouton)
+
+			#Evenement lors du click sur OK
+			ok.signal_connect("clicked"){
+				if (b5.active?)
+					@tailleNouvelleMatrice = 5
+				elsif (b10.active?)
+					@tailleNouvelleMatrice = 10
+				elsif (b15.active?)
+					@tailleNouvelleMatrice = 15
+				elsif (b20.active?)
+					@tailleNouvelleMatrice = 20
+				elsif (b25.active?)
+					@tailleNouvelleMatrice = 25			
+				end				
+							
+				#On crée un nouveau jeu de picross avec pour taille, la taille du radiobutton selectionné
+				#Il faudra donc surcharger le constructeur de gui pour pouvoir y ajouter une taille de matrice
+				print "Lancement de la nouvelle grille de taille : ", @tailleNouvelleMatrice, "\n"
+				Gui.lancerTailleChoisie("Picross", @tailleNouvelleMatrice)
+
+			}
 
 
 			popupTailleGrille.add(Frame.new.add(vb))
