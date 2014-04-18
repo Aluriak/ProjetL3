d4f95aba608d0248970c9bcbf528171b7b0a2ab3
Aluriak <lucas.bourneuf@laposte.net>
2014-04-18 16:13:51 +0200

    Génération de grilles aléatoires

--- src/grille/grille.rb
+++ src/grille/grille.rb
@@ -32,11 +32,16 @@ class Grille
   ##
   # Attend la taille de la grille.
   # Si les matrices de lignes et colonnes sont renseignées, elles sont utilisées telles quelles
-  # Sinon, elles sont remplacées par des matrices sans entiers.
+  # Sinon, elles sont remplacées par des matrices initialisées aléatoirement.
   def initialize(taille, matriceDesLignes = nil, matriceDesColonnes = nil)
     # matriceDesLignes[3][4] accède au 5ème chiffre de la 4ème ligne
     if matriceDesLignes == nil then 
       @matriceDesLignes = Array.new(taille) { Array.new }
+      @matriceDesLignes.each do |lne|
+      	(1+rand((taille/2).to_i)).times do 
+      	  lne.push(rand(1)+1)
+	end
+      end
     else
       @matriceDesLignes = matriceDesLignes
     end
@@ -44,6 +49,11 @@ class Grille
     # matriceDesColonnes[0][10] accède au 11ème chiffre de la 1ère ligne
     if matriceDesColonnes == nil then 
       @matriceDesColonnes = Array.new(taille) { Array.new } 
+      @matriceDesColonnes.each do |col|
+      	(1+rand((taille/2).to_i)).times do 
+      	  col.push(rand(1)+1)
+	end
+      end
     else
       @matriceDesColonnes = matriceDesColonnes
     end
