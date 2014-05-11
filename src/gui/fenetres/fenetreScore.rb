b68d2742f3cbc95bb47cca05e0ba9bb39b5832b4
Aluriak <lucas.bourneuf@laposte.net>
2014-05-11 20:09:21 +0200

    Description du contenu du fichier ajoutée

--- src/gui/fenetres/fenetreScore.rb
+++ src/gui/fenetres/fenetreScore.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
-# FILENAME
-# description of content
+# FENETRESCORE.RB
+# Définition de la fenêtre de scores affichant les scores pour une grille particulière.
 # BOURNEUF
 #
 
