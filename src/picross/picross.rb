a14b8c16a33298613af66e73c2450edeeb17116b
Aluriak <lucas.bourneuf@laposte.net>
2014-05-11 16:13:21 +0200

    Elagage : les fonctions inutiles du picross sont retirées

--- src/picross/picross.rb
+++ src/picross/picross.rb
@@ -20,7 +20,6 @@ load "src/grille/jouable.rb"
 class Picross
   @config
   @grille
-  @registreTemporaire
   @gestionnaireDeSauvegarde
 
   # Référence vers une instance de classe Configuration
@@ -31,6 +30,8 @@ class Picross
   #attr_reader :gestionnaireDeSauvegarde
 
 
+
+
   def initialize
     # Chargement de la Configuration
     @config = Configuration.charger
@@ -39,23 +40,6 @@ class Picross
   end
 
 
-  # Donne un id attribuable à une grille
-  # la configuration est modifiée et sauvée par cette méthode
-  def idGrilleSuivant
-    id = @config.idGrilleSuivant
-    self.sauverConfiguration
-    return id
-  end
-
-
-  # Sauve la configuration actuelle dans le fichier de configuration,
-  # écrasant de fait les anciennes valeurs
-  def sauverConfiguration
-    File.open(CONSTANT_FICHIER_DATA_CONFIG, "w") do |f|
-      f.puts Marshal.dump(@config)
-    end
-    return self
-  end
 
 
   # Création d'une grille racine à partir de la table d'état passée en argument.
@@ -74,7 +58,9 @@ class Picross
 
     # Enregistrer la grille racine
     @gestionnaireDeSauvegarde.ajouterGrilleRacine(grilleRacine)
+    @gestionnaireDeSauvegarde.sauvegarderGrillesRacines
   end
+
 
 
 
@@ -83,6 +69,7 @@ class Picross
   def derniereTailleDeGrille()
     return @config.derniereTailleGrille
   end
+
 
 
 
@@ -96,75 +83,25 @@ class Picross
       grille = listGrlRacine.choice # prendre une grille au hasard dans la liste.
     end
     @grille = GrilleJouable.creerDepuis(grille)
+    @config.derniereTailleGrille = taille
+    @config.sauvegarder
     return nil
   end
 
 
-  ##
-  # Retourne l'état de la grille aux coordonnées demandées
-  def etatGrille(i, j)
-    raise "coordonnée i non valide." if i >= 0 and i < @grille.taille
-    raise "coordonnée j non valide." if j >= 0 and j < @grille.taille
-    return @grille.matriceDeJeu[i][j]
-  end
-
-
-  ##
-  # Bascule l'état de la grille aux coordonnées données à l'état donné,
-  # ou au suivant si l'état n'est pas communiqué
-  def basculerEtat(i, j, etat = nil)
-    @grille.basculer(i, j, etat)
-  end
 
 
   ##
   # Sauvegarde la grille actuellement jouée dans une nouvelle sauvegarde.
   # Le nom de sauvegarde permet d'identifier la sauvegarde.
-  def sauverGrilleCommeGrilleJouable(nom_sauvegarde = nil)
+  def sauverGrilleJouable(nom_sauvegarde = nil)
     @grille.genererNom(nom_sauvegarde)
     @gestionnaireDeSauvegarde.ajouterGrilleJouable(@grille)
     @gestionnaireDeSauvegarde.sauvegarderGrillesJouables
   end
 
 
-  ##
-  # Sauvegarde la grille actuellement jouée comme grille racine.
-  def sauverGrilleCommeGrilleRacine()
-    grille = GrilleRacine.creerDepuis(@grille)
-    @gestionnaireDeSauvegarde.ajouterGrilleRacine(grille)
-    @gestionnaireDeSauvegarde.sauvegarderGrillesRacines
-    return nil
-  end
 
-
-  ##
-  # Sauvegarde la grille actuelle dans un registre temporaire non persistant.
-  # Ecrase une éventuelle ancienne sauvegarde, ne modifie pas la grille.
-  def sauverGrilleEnRegistreTmp()
-    @registreTemporaire = @grille
-  end
-
-
-  ##
-  # Charge dans la grille le contenu du registre temporaire
-  # Ecrase le contenu précédent de la grille, ne modifie pas le registre.
-  def chargerRegistreTemporaire()
-    @grille = @registreTemporaire
-  end
-
-
-  ##
-  # Predicat: Vrai si la @grille correspond au facteurs.
-  def grilleTerminee?()
-    return @grille.terminee?    
-  end
-
-
-  ##
-  # Predicat: Vrai si t désigne une taille de grille valide, faux sinon.
-  def tailleGrilleValide?(t)
-    return @@tailles.include?(t)
-  end
 
 end # end class
 
