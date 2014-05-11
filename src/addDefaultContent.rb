c6cf8e35fd9abf5307645b493a602dc74f486168
Aluriak <lucas.bourneuf@laposte.net>
2014-05-11 19:25:24 +0200

    Mise en place de scores par défaut

--- src/addDefaultContent.rb
+++ src/addDefaultContent.rb
@@ -5,6 +5,7 @@
 # - add some basic contents;
 #
 load "src/commun/commun.rb"
+load "src/scores/scores.rb"
 load "src/configuration/configuration.rb"
 load "src/gestionnaireDeSauvegarde/gestionnaireDeSauvegarde.rb"
 load "src/grille/jouable.rb"
@@ -12,15 +13,19 @@ load "src/grille/racine.rb"
 
 
 # LOAD
-gds = GestionnaireDeSauvegarde.new
-cnf = Configuration.new(5)
+gds = GestionnaireDeSauvegarde.new(true)
+cnf = Configuration.new(5, Scores.creer())
 
 
 # FILLING
 cnf.derniereTailleGrille=5
 
 # create grille racine and add it to gds
-gds.ajouterGrilleRacine(GrilleRacine.deTaille(5, "startup"))
+nom_grille = "Mon premier picross"
+gds.ajouterGrilleRacine(GrilleRacine.deTaille(5, nom_grille))
+cnf.scores.ajouterScoreAGrille(nom_grille, Score.creer(5, 104, 2), "micheline")
+cnf.scores.ajouterScoreAGrille(nom_grille, Score.creer(5, 167, 2), "michel")
+cnf.scores.ajouterScoreAGrille(nom_grille, Score.creer(5, 642, 1), "roger")
 
 
 
