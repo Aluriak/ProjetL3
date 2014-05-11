42bc20ad8bc926d381b15a4af0618a28d5f27989
Aluriak <lucas.bourneuf@laposte.net>
2014-05-11 20:44:15 +0200

    Fix indentation to 2 spaces

--- src/gui/fenetres/fenetreScore.rb
+++ src/gui/fenetres/fenetreScore.rb
@@ -19,36 +19,35 @@ require "glib2"
 # mainteneur : BOURNEUF
 class FenetreScore < Gtk::Window
 
-	def initialize(parent, scores_globaux, nom_grille)
+  def initialize(parent, scores_globaux, nom_grille)
+    super("Scores")
+    self.signal_connect("destroy") { self.destroy }
+    set_resizable(true)
 
-		super("Scores")
-		self.signal_connect("destroy") { self.destroy }
-		set_resizable(true)
+    vbox = Gtk::VBox.new(10)
+    vbox.pack_start(Gtk::Label.new("Meilleurs scores pour la grille "+nom_grille), true, true)
 
-                vbox = Gtk::VBox.new(10)
-                vbox.pack_start(Gtk::Label.new("Meilleurs scores pour la grille "+nom_grille), true, true)
-
-                # Obtention de tous les scores
-                # Et ajouts dans la vbox
-                grille_scores = scores_globaux.scoresDeGrille(nom_grille)
-                grille_scores.each do |score|
-                  profil_nom, profil_score = score
-                  hbox = Gtk::HBox.new
-                  hbox.pack_start(Gtk::Label.new(profil_nom))
-                  hbox.pack_start(Gtk::Label.new(profil_score.to_s))
-                  vbox.pack_start(hbox, true, true)
-                end
-
+    # Obtention de tous les scores
+    # Et ajouts dans la vbox
+    grille_scores = scores_globaux.scoresDeGrille(nom_grille)
+    grille_scores.each do |score|
+      profil_nom, profil_score = score
+      hbox = Gtk::HBox.new
+      hbox.pack_start(Gtk::Label.new(profil_nom))
+      hbox.pack_start(Gtk::Label.new(profil_score.to_s))
+      vbox.pack_start(hbox, true, true)
+    end
 
 
-                self.add(vbox)
 
-                
-                self.set_modal(true)            # this window block interactions with 
-                self.set_transient_for(parent)  # parent window.
-                self.show_all
+    self.add(vbox)
 
-	end
+    
+    self.set_modal(true)            # this window block interactions with 
+    self.set_transient_for(parent)  # parent window.
+    self.show_all
+
+  end
 
 end
 
