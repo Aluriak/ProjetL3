c551720b9d5bd5355dc20227e5a7f3d9282b1ab6
Aluriak <lucas.bourneuf@laposte.net>
2014-05-11 19:17:03 +0200

    Nouvelle structure + affichage des scores effectif

--- src/gui/fenetres/fenetreScore.rb
+++ src/gui/fenetres/fenetreScore.rb
@@ -1,26 +1,57 @@
+# -*- encoding: utf-8 -*-
+# FILENAME
+# description of content
+# BOURNEUF
+#
 
 			
+#################################
+# IMPORTS			#
+#################################
 require "gtk2"
 require "glib2"
-include Gtk
 
-class FenetreScore
 
-	@score
-	
-	def initialize(score)
 
-		@score = score
+#################################
+# FENETRE SCORE			#
+#################################
+# mainteneur : BOURNEUF
+class FenetreScore < Gtk::Window
 
-		dialog = MessageDialog.new(nil, 
-			Dialog::DESTROY_WITH_PARENT,
-			MessageDialog::INFO,
-			MessageDialog::BUTTONS_CLOSE,
-			"Voici les scores:\nToto - 4\nJaco -- -2\nAl Acampagne -- -1\n")
-		dialog.run
-		dialog.destroy
+	def initialize(parent, scores_globaux, nom_grille)
 
+		super("Scores")
+		self.signal_connect("destroy") { self.destroy }
+		set_resizable(true)
+
+                vbox = Gtk::VBox.new(10)
+                vbox.pack_start(Gtk::Label.new("Meilleurs scores pour la grille "+nom_grille), true, true)
+
+                # Obtention de tous les scores
+                # Et ajouts dans la vbox
+                grille_scores = scores_globaux.scoresDeGrille(nom_grille)
+                grille_scores.each do |score|
+                  profil_nom, profil_score = score
+                  hbox = Gtk::HBox.new
+                  hbox.pack_start(Gtk::Label.new(profil_nom))
+                  hbox.pack_start(Gtk::Label.new(profil_score.to_s))
+                  vbox.pack_start(hbox, true, true)
+                end
+
+
+
+                self.add(vbox)
+
+                
+                self.set_modal(true)            # this window block interactions with 
+                self.set_transient_for(parent)  # parent window.
+                self.show_all
 
 	end
 
 end
+
+
+
+
