bbb4753a05549bbb975cf3ace1323f3c815b091c
LeCroissant <nico22330@gmail.com>
2014-05-13 22:30:17 +0200

    Modification du selecteur de grille enregistrée

--- src/gui/fenetres/fenetreCharger.rb
+++ src/gui/fenetres/fenetreCharger.rb
@@ -7,22 +7,48 @@ include Gtk
 class FenetreCharger
 
 
-	def initialize()
-
-
-		dialog = FileChooserDialog.new("Charger une grille",
-			nil,
-			FileChooser::ACTION_OPEN,
-			nil,
-			[Stock::CANCEL, Dialog::RESPONSE_CANCEL],
-			[Stock::OPEN, Dialog::RESPONSE_ACCEPT]
-		)
-
-		if dialog.run == Dialog::RESPONSE_ACCEPT
-			puts "filename = #{dialog.filename}"
+	def initialize(picross)
+		window = Window.new("Charger une partie")
+		window.add(vb = VBox.new)
+		
+    cbParties = ComboBox.new
+    cbParties.signal_connect "changed" do |w, e|
+    	on_changed w, e, label
 		end
-		dialog.destroy
 
+		
+
+    #picross.gestionnaireDeSauvegarde.each { |nom| @combo_profils.append_text(nom) }
+    buttonValider = Button.new("Valider")
+    labelTaille = Label.new("Taille de la grille")
+    cbTailles = ComboBox.new
+
+    vb.pack_start(labelTaille)
+    vb.pack_start(cbTailles)
+    vb.pack_start(cbParties)
+    vb.pack_start(buttonValider)
+
+    cbParties.set_visible(false)
+    cbParties.signal_connect "changed" do |w, e|
+    	#on_changed w, e, label
+    	cbParties.set_visible(true)
+		end
+
+
+    cbTailles.append_text("5")
+    cbTailles.append_text("10")
+    cbTailles.append_text("15")
+    cbTailles.append_text("20")
+
+    
+
+    buttonValider.signal_connect("clicked"){
+    	#picross.gestionnaireDeSauvegarde.chargerGrillesJouables()
+    }
+
+
+    window.set_resizable(false)
+    window.show_all
 
 	end
 
