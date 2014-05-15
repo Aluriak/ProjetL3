2ddedbaac23d6937926aa0293453deef48befe71
LeCroissant <nico22330@gmail.com>
2014-05-15 21:11:49 +0200

    Changement type fenetre

--- src/gui/fenetres/fenetreSauvegarderAvantQuitter.rb
+++ src/gui/fenetres/fenetreSauvegarderAvantQuitter.rb
@@ -24,40 +24,49 @@ include Gtk
 class FenetreSauvegarderAvantQuitter < Window
 
 
-	def initialize(picross, timer)
+	 # impossible de créer une instance de classe
+  private_class_method :new
 
 
-		super("Sauvegarder la grille courante ?")
-		self.add(vbox = VBox.new)
+  
+  def FenetreSauvegarderAvantQuitter.show(picross, timer, parent)
+    operation_choisie = Dialog::RESPONSE_OK
 
-		vbox.pack_start(Label.new("Attention vos modifications sur la grille courante seront perdues\nsi vous ne la sauvegardez pas."))
-		
-		vbox.pack_start(hbox = HBox.new)
+    # vérification de création de profil
+    dialog = Dialog.new(
+      "Sauvegarder la grille courante avant de quitter? ", 
+      parent, 
+      Dialog::DESTROY_WITH_PARENT | Dialog::MODAL)
+    dialog.vbox.pack_start(Label.new("Attention vos modifications sur la grille courante seront perdues\nsi vous ne la sauvegardez pas."))
+    dialog.vbox.pack_start(hbox = HBox.new)
 
-		boutonSauvegarder = Button.new("Sauvegarder")
-		boutonQuitterSansSauvegarder = Button.new("Fermer sans sauvegarder")
-		boutonAnnuler = Button.new("Annuler")
+    hbox.pack_start(btnQuit = Button.new("Quitter sans sauvegarder"))
+    hbox.pack_start(btnAnnuler = Button.new("Annuler"))
+    hbox.pack_start(btnSauvegarder = Button.new("Sauvegarder"))
+   	
+    
+    btnQuit.signal_connect("clicked"){
+    	puts "Quit"
 
-		hbox.pack_start(boutonQuitterSansSauvegarder)
-		hbox.pack_start(boutonAnnuler)
-		hbox.pack_start(boutonSauvegarder)
+    }
+    btnAnnuler.signal_connect("clicked"){
+    	puts "Annuler"
+    }
+    btnSauvegarder.signal_connect("clicked"){
+    	puts "Save"
+    }
 
-		boutonQuitterSansSauvegarder.signal_connect("clicked"){Gtk.main_quit}
-		boutonAnnuler.signal_connect("clicked"){self.destroy}
-		boutonSauvegarder.signal_connect("clicked"){FenetreSauvegarde.new(picross, timer.sec)}
-		boutonSauvegarder.signal_connect("clicked"){self.destroy}
-
-		
-		
-		self.set_modal(true)
-		self.set_window_position(:center)
-		self.set_default_size(150,100)
-		self.show_all
+    dialog.set_resizable(false)
+    dialog.show_all
+    dialog.run
+    dialog.destroy
 
 
+    
+  end
 
 
-	end	
+
 
 end	
 
