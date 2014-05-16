4f3eb96fec177442d9ee9816ac9b8d6eacfb294d
Grainblanc <simonin_ewen@hotmail.fr>
2014-05-16 15:58:08 +0200

    indentation

--- src/gui/chronometre.rb
+++ src/gui/chronometre.rb
@@ -20,91 +20,87 @@
 # Nécessite une référence à un Gtk::Label, qui sera mis à jour chaque seconde.
 # Affiche le temps au format H:M:S
 class Chronometre
-  @reflabel
-  @sec
-  @pause
-  @chrono
-  @dixieme
+	@reflabel
+	@sec
+	@pause
+	@chrono
+	@dixieme
 
 
-  # nombre de seconde écoulée depuis la dernière remise à zéro
-  attr_reader :sec
+	# nombre de seconde écoulée depuis la dernière remise à zéro
+	attr_reader :sec
 
-  def initialize(ref_label, temps_ecoule = 0)
-    @sec = temps_ecoule
-    @dixieme = 0      
-    @ref_label = ref_label
-    @pause = true
-    majlabel
-  end
+	def initialize(ref_label, temps_ecoule = 0)
+		@sec = temps_ecoule
+		@dixieme = 0      
+		@ref_label = ref_label
+		@pause = true
+		majlabel
+	end
 
-  ##
-  # Relance le chronomètre
-  def start
-    if @pause 
-      @pause = false
-      @chrono = Thread.new { self.run }
-    end
-  end
+	##
+	# Relance le chronomètre
+	def start
+		if @pause 
+		@pause = false
+		@chrono = Thread.new { self.run }
+		end
+	end
 
-  ##
-  # Stoppe le chronomètre
-  def stop
-    if not @pause then 
-      @pause = true
-      @chrono.join
-    end
-  end
+	##
+	# Stoppe le chronomètre
+	def stop
+		if not @pause then 
+		@pause = true
+		@chrono.join
+		end
+	end
 
-  ##
-  # Remet le chronomètre à zéro.
-  def raz
-        @sec = 0
-        @dixieme = 0
-        majlabel
-  end
+	##
+	# Remet le chronomètre à zéro.
+	def raz
+			@sec = 0
+			@dixieme = 0
+			majlabel
+	end
 
-  ##
-  # Predicat vrai si chronomètre actuellement en pause.
-  def paused?
-    return @pause
-  end
+	##
+	# Predicat vrai si chronomètre actuellement en pause.
+	def paused?
+		return @pause
+	end
 
-
-  ##
-  # Compte les secondes et met le label référencé à jour
-  def run
-    # garder en mémoire les dixièmes de seconde permet de diminuer la perte de temps induite par la pause.
-    while not @pause do 
-      if @dixieme == 10 then
-        @dixieme = 0
-        @sec += 1
-        majlabel
-      end
-      @dixieme += 1
-      sleep(0.1)
-    end
-  end
-
-
-  ##
-  # Met à jour le texte du label.
-  def majlabel
-    @ref_label.set_text(self.to_s)
-  end 
-  private :majlabel
+	##
+	# Compte les secondes et met le label référencé à jour
+	def run
+		# garder en mémoire les dixièmes de seconde permet de diminuer la perte de temps induite par la pause.
+		while not @pause do 
+		if @dixieme == 10 then
+			@dixieme = 0
+			@sec += 1
+			majlabel
+		end
+		@dixieme += 1
+		sleep(0.1)
+		end
+	end
 
 
+	##
+	# Met à jour le texte du label.
+	def majlabel
+		@ref_label.set_text(self.to_s)
+	end 
+	private :majlabel
 
-
-  ##
-  # Affichage du temps en human-readable
-  def to_s
-    s, m = @sec % 60, @sec / 60
-    h = m / 60
-    m = m % 60
-    return ("%02d:%02d:%02d" % [h, m, s])
-  end
+	
+	# Affichage du temps en human-readable
+	def to_s
+		s, m = @sec % 60, @sec / 60
+		h = m / 60
+		m = m % 60
+		return ("%02d:%02d:%02d" % [h, m, s])
+	end
 end
 
 
--- src/gui/confirmerNouveauProfil.rb
+++ src/gui/confirmerNouveauProfil.rb
@@ -21,31 +21,31 @@ include Gtk
 class ConfirmerNouveauProfil
 
   # impossible de créer une instance de classe
-  private_class_method :new
+	private_class_method :new
 
 
-  ## 
-  # prend une instance de classe Picross en paramètre.
-  # Retourne vrai si l'utilisateur souhaite créer le profil
-  def ConfirmerNouveauProfil.show(parent, nom_profil)
-    operation_choisie = Dialog::RESPONSE_OK
+	## 
+	# prend une instance de classe Picross en paramètre.
+	# Retourne vrai si l'utilisateur souhaite créer le profil
+	def ConfirmerNouveauProfil.show(parent, nom_profil)
+		operation_choisie = Dialog::RESPONSE_OK
 
-    # vérification de création de profil
-    dialog = Dialog.new(
-      "Création de profil", 
-      parent,
-      Dialog::DESTROY_WITH_PARENT | Dialog::MODAL,
-      [Stock::OK, Dialog::RESPONSE_OK], 
-      [Stock::CANCEL, Dialog::RESPONSE_CANCEL]
-    )
-    dialog.vbox.add(Label.new("Créer le profil " + nom_profil + " ?"))
-    dialog.signal_connect("response") { |fenetre, id_rep| operation_choisie = id_rep }
-    dialog.show_all
-    dialog.run
-    dialog.destroy
+		# vérification de création de profil
+		dialog = Dialog.new(
+			"Création de profil", 
+			parent,
+			Dialog::DESTROY_WITH_PARENT | Dialog::MODAL,
+			[Stock::OK, Dialog::RESPONSE_OK], 
+			[Stock::CANCEL, Dialog::RESPONSE_CANCEL]
+		)
+		dialog.vbox.add(Label.new("Créer le profil " + nom_profil + " ?"))
+		dialog.signal_connect("response") { |fenetre, id_rep| operation_choisie = id_rep }
+		dialog.show_all
+		dialog.run
+		dialog.destroy
 
-    return (operation_choisie == Dialog::RESPONSE_OK)
-  end
+		return (operation_choisie == Dialog::RESPONSE_OK)
+	end
 end
 
 
